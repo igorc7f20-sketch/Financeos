92a6ccf2e68ede5cf91b143130332e1096b91ed0
igorc7f20-sketch <igorc7f20@gmail.com>
2026-05-05 23:18:18 -0400

    fix: corret import name for token refresh view.

--- backend/apps/users/urls.py
+++ backend/apps/users/urls.py
@@ -1,9 +1,9 @@
 from django.urls import path
-from .views import RegisterView, ProfileView, LoginView, RefreshView
+from .views import RegisterView, ProfileView, LoginView, RefreshTokenView
 
 urlpatterns = [
     path("register/", RegisterView.as_view(), name="auth-register"),
     path("login/", LoginView.as_view(), name="auth-login"),
     path("profile/", ProfileView.as_view(), name="auth-profile"),
-    path("refresh/", RefreshView.as_view(), name="auth-refresh"),
+    path("refresh/", RefreshTokenView.as_view(), name="auth-refresh"),
 ]
