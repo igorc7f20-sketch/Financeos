c0345b75e0e4aaf31b1d4f15671efb2a90d763e7
igorc7f20-sketch <igorc7f20@gmail.com>
2026-05-21 18:42:18 -0400

    fix: delete line 27, whitespace.

--- backend/apps/transactions/services.py
+++ backend/apps/transactions/services.py
@@ -24,7 +24,6 @@ class CategoryService:
             raise ServiceException(
                 f"Category '{name}' of type '{type}' already exists."
             )
-        
         return CategoryRepository.create_for_user(
             user, name=name, type=type, color=color, icon=icon
         )
