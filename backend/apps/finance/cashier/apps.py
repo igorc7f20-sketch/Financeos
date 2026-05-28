5ddf7d5a9e5fcc314a9b725ac717a51390e8d745
igorc7f20-sketch <igorc7f20@gmail.com>
2026-05-28 18:52:47 -0400

    fix: remove finance to the backend.

--- backend/apps/finance/cashier/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class CashierConfig(AppConfig):
-    default_auto_field = "django.db.models.BigAutoField"
-    name = "apps.finance.cashier"
--- backend/apps/finance/cashier/urls.py
+++
@@ -1,7 +0,0 @@
-from rest_framework.routers import DefaultRouter
-from .views import CashEntryViewSet
-
-router = DefaultRouter()
-router.register(r"cashier", CashEntryViewSet, basename="cashier")
-
-urlpatterns = router.urls
